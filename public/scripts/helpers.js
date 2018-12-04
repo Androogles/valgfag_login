c9fb840d5ddc477688c2062a800ae28b4a098360
Andreas Christian Kraft <2873573@RTS-365.dk>
2018-12-04 14:56:21 +0100

    password meter tilføjet, script hentes i template

--- public/scripts/helpers.js
+++ public/scripts/helpers.js
@@ -1,3 +1,4 @@
+// fucntion med regular expression til at validere på email   
 function validateEmail(email_check) {
     var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email_check);
